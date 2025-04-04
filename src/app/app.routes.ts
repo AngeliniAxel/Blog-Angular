d71a39ff73b0f5c7cd47cd227b830e8d5b181f6a
AngeliniAxel <axel.angelini@gmail.com>
2025-04-04 08:57:15 +0200

    link to ViewPost component in every card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NewComponent } from './pages/new/new.component';
+import { ViewPostComponent } from './pages/view-post/view-post.component';
 
 export const routes: Routes = [
   { path: 'home', pathMatch: 'full', component: HomeComponent },
   { path: 'new', component: NewComponent },
+  { path: 'post/:idpost', component: ViewPostComponent },
   { path: '**', redirectTo: '/home' },
 ];
--- src/app/components/card-post/card-post.component.ts
+++ src/app/components/card-post/card-post.component.ts
@@ -1,13 +1,16 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Post } from '../../interfaces/post.interface';
 
 @Component({
   selector: 'app-card-post',
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   templateUrl: './card-post.component.html',
   styleUrl: './card-post.component.css',
 })
 export class CardPostComponent {
   @Input() post: Post | undefined;
+
+  goToPost(postId: number) {}
 }
