b81aab9b3f184bbde0e794d116a1f5e9da77b085
AngeliniAxel <axel.angelini@gmail.com>
2025-04-05 21:03:20 +0200

    Added FromFroup and onSubmit

--- src/app/pages/new/new.component.ts
+++ src/app/pages/new/new.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ICategory } from '../../interfaces/category.interface';
 import { CategoryService } from '../../services/category.service';
 
@@ -15,5 +15,22 @@ export class NewComponent {
 
   ngOnInit() {
     this.categories = this.categoriesService.getAll();
+  }
+
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      title: new FormControl(),
+      text: new FormControl(),
+      author: new FormControl(),
+      image: new FormControl(),
+      category: new FormControl(),
+    });
+  }
+
+  onSubmit() {
+    const { title, text, author, image, category } = this.form.value;
+    console.log({ ...this.form.value, date: new Date() });
   }
 }
