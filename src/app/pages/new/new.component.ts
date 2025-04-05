ad86324a17242e736fba405211d1a3cc04223222
AngeliniAxel <axel.angelini@gmail.com>
2025-04-05 19:26:50 +0200

    added form html structure

--- src/app/pages/new/new.component.ts
+++ src/app/pages/new/new.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ICategory } from '../../interfaces/category.interface';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-new',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new.component.html',
-  styleUrl: './new.component.css'
+  styleUrl: './new.component.css',
 })
 export class NewComponent {
+  categories: ICategory[] = [];
+  categoriesService = inject(CategoryService);
 
+  ngOnInit() {
+    this.categories = this.categoriesService.getAll();
+  }
 }
